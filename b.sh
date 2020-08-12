e5e4135ca943f6ee880f637a897fafc5d0801e9d
rangapv <rangapv@yahoo.com>
2020-08-12 06:48:49 +0000

    Added support for image and container removal

--- b.sh
+++ b.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "Docker System Cleanup Script"
-
+echo "Usuage: ./b.sh iamge_id : to remove only  the images IF no container is using this image"
+echo "Usuage: ./b.sh -r image_id : to list the container which has these image dependecy and remove the container as well"
+echo "usuage: ./b.sh : to list all images and containers running in the system and ask to delete per image"
 doker_all(){
 echo "The docker containers in this SYSTEMS are:"
 while read -r line
@@ -41,11 +43,31 @@ res1=`docker image rm $1`
   fi
 }
 
+
+image_container_match(){
+test1=`docker image ls | grep $1`
+test2=`echo "$test1" | awk '{print $1}'`
+test=`docker container ls | grep "$test2"`
+if [ ! -z "$test" ]
+then
+echo "The containers that are suing the image-ID $1 are : $test"
+else
+echo "No container is using this image $1"
+fi
+}
 #Main
 
 if [ $# != 0 ]
 then
+  if [ $1 == "-r" ]
+  then
+    image_container_match $2
+  else
+  # echo "Acceptable commands are -r IMAGE_ID"
   image_list ubuntu
+  break;
+  fi
+
 elif [ $# == 0 ]
 then
   doker_all
